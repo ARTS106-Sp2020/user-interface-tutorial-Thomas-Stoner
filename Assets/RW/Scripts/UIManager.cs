a0a8dfbbb176861df56bde4c0c2a49f4015774ca
Thomas-Stoner <61507770+Thomas-Stoner@users.noreply.github.com>
2020-05-08 22:50:55 -0400

    Added Close Button

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -7,11 +7,19 @@ public class UIManager : MonoBehaviour
 {
     public Animator startButton;
     public Animator settingsButton;
+    public Animator dialog;
 
   public void OpenSettings()
     {
         startButton.SetBool("isHidden", true);
         settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+    public void ClosesSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
   public void StartGame()
     {
